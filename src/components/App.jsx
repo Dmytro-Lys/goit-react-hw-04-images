23faf721e3a5dc751d56e75aa10e0edf490ab616
Dmytro-Lys <d.lysenko122@gmail.com>
2023-07-28 17:14:59 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,7 +40,8 @@ const App = () => {
       e.preventDefault()
       if (!querry.trim()) return Notiflix.Notify.failure(`Fill the search field`);
       resetSearch()
-    }
+  }
+  
 // fetch
   const loadMore = () => {
     setPage(prev => prev + 1)
@@ -66,6 +67,7 @@ const App = () => {
         setIsLoading( false);
       }
   }
+
   // Modal
   const imageClick = ({ target: { dataset: { large }, alt } }) => {
     if (!large) return
@@ -109,102 +111,5 @@ const App = () => {
        </div>
      )
 }
-// class App extends Component {
-//   state = {
-//     images: [],
-//     page: 1,
-//     querry: '',
-//     maxPage: 0,
-//     isLoading: false,
-//     showImage: { largeImageURL: "", tags: "" },
-//     refModal: React.createRef(),
-//     refLastElem: React.createRef(),
-//     isShowModal: false
-//   }
-   
-
-//     handleChange = e => {
-//       const { name, value } = e.target;
-//       this.setState({ [name]: value.trim() });
-//   }
-  
-//    handleSubmit = e => {
-//       e.preventDefault()
-//      const { querry } = this.state
-//      if (!querry.trim()) return Notiflix.Notify.failure(`Fill the search field`);
-//       this.resetSearch()
-//     }
-  
-//    resetSearch = () => this.setState({images: [], page: 1, maxPage: 0, isLoading: true })
-    
-   
-//   loadMore = () => {
-//     this.setState(prev => {
-//       return {page: prev.page + 1, isLoading: true}
-//     })
-//   }
-
-//   onError = err => Notiflix.Notify.failure(err.message)
-
-//   async componentDidUpdate(prevProps, prevState) {
-   
-//     const { querry, page, maxPage, isLoading, images, isShowModal, refModal, refLastElem } = this.state
-//     // set focus to Modal for onKeyDown
-//     if (isShowModal) refModal.current.focus()
-//     // fetching new images
-//     if (isLoading)  {
-//       try {
-//          const data = await getImages(querry, page);
-//         if (!data.hits.length) throw new Error("Sorry, there are no images matching your search query. Please try again.");
-//         const imagesPage = this.generateGalleryItems(data.hits)
-//         this.setState({ images: [...images, ...imagesPage] })
-//         if (maxPage === 0) this.setState({ maxPage: Math.ceil(data.totalHits / 12) })
-//       } catch (error) {
-//         this.onError(error)
-//       } finally {
-//         this.setState({ isLoading: false });
-//       }
-//     }
-//     // scroll
-//     const prevImages = prevState.images
-//     if (images.length>0 && prevImages.length !== images.length) refLastElem.current.scrollIntoView({ behavior: 'smooth' })
-//   }
-
-//   generateGalleryItems = data => data.map(({ webformatURL, tags, largeImageURL }) => {
-//     return { id: nanoid(), webformatURL, tags, largeImageURL }
-//   })
-
-  
-//   imageClick = ({ target: { dataset: { large }, alt } }) => {
-//     if (!large) return
-//     const imageOptions = { largeImageURL: large, tags: alt }
-//     this.setState({ showImage: imageOptions, isShowModal: true })
-//   }
-
-//   closeModal = () => this.setState({ isShowModal: false })
-
-//   modalClick = (e) => {
-//     if (!e.target.src)  this.closeModal()
-//   }
-
-//   handleKeyDown = e => {
-//     if (e.key === "Escape") this.closeModal()
-//   }
-  
- 
-//   render() {
-//     const { querry, page, maxPage, isLoading, images, isShowModal, showImage, refModal, refLastElem } = this.state
-//     return (
-//        <div className={css.App}>
-//         <Searchbar querry={querry} onChange={this.handleChange} onSubmit={this.handleSubmit}/>
-//         {images.length > 0 && <ImageGallery images={images} onClick={this.imageClick} refLastElem={refLastElem} />}
-//          <Loader render={isLoading} />
-//         {page < maxPage && <Button onClick={this.loadMore} />}
-//         {isShowModal && <Modal imageOptions={showImage} refModal={refModal} onClick={this.modalClick} onKeyDown={this.handleKeyDown} />}
-//        </div>
-//      )
-//    }
-  
-// }
 
 export default App
